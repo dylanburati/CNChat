3e7dc9177aee039e61d850cc099044a59dfa28be
dylanburati <bd184191@cpslions.com>
2016-12-30 22:26:18 -0500

    s/StringBuffer/StringBuilder/g

--- CryptoCheck.java
+++ CryptoCheck.java
@@ -100,7 +100,7 @@ public class CryptoCheck {
         return b256;
     }
 
-    private static void byte2hex(byte b, StringBuffer buf) {
+    private static void byte2hex(byte b, StringBuilder buf) {
         char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
         int high = ((b & 240) >> 4);
         int low = (b & 15);
@@ -109,7 +109,7 @@ public class CryptoCheck {
     }
 
     private static String toHexString(byte[] block) {
-        StringBuffer buf = new StringBuffer();
+        StringBuilder buf = new StringBuilder();
         int len = block.length;
         for (int i = 0; i < len; i++) {
             byte2hex(block[i], buf);
