87cdd5d5e14e339c7c900c6ed779136437002c0e
Dylan Burati <dylanburati@protonmail.com>
2017-04-25 00:13:16 -0400

    It's a hollow play, but they'll clap anyway.

--- CryptoCheck.java
+++ CryptoCheck.java
@@ -100,19 +100,11 @@ public class CryptoCheck {
         return b256;
     }
 
-    private static void byte2hex(byte b, StringBuilder buf) {
-        char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-        int high = ((b & 240) >> 4);
-        int low = (b & 15);
-        buf.append(hexChars[high]);
-        buf.append(hexChars[low]);
-    }
-
     private static String toHexString(byte[] block) {
         StringBuilder buf = new StringBuilder();
         int len = block.length;
         for (int i = 0; i < len; i++) {
-            byte2hex(block[i], buf);
+            buf.append(String.format("%02x", block[i] & 255));
             if (i < len-1) buf.append(":");
         }
         return buf.toString();
