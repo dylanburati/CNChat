85e8845445d1cd20be9f99e901692c807c086072
Dylan Burati <dylanburati@protonmail.com>
2019-03-09 03:19:09 -0500

    Split commandHandlers into internal routines and custom callbacks

--- ChatClient/src/ChatClient.js
+++ ChatClient/src/ChatClient.js
@@ -1,10 +1,170 @@
 /* global window WebSocket axios */
+const commandHandlers = {
+  conversation_request(commandResults, session) {
+    const conversationRequest = JSON.parse(commandResults);
+    session.addConversationFromRequest(conversationRequest).then(toAdd => {
+      session.enqueue('conversation_add ' + JSON.stringify(toAdd), 0);
+    });
+  },
+
+  conversation_ls(commandResults, session) {
+    const conversationLS = JSON.parse(commandResults);
+    const keysToRequest = [];
+    const promises = [];
+    conversationLS.forEach(conversationObj => {
+      const promise = session.addConversationFromLS(conversationObj).then(conversationStatus => {
+        if(conversationStatus === false) {
+          if(keysToRequest.indexOf(conversationObj.role1) === -1) {
+            keysToRequest.push(conversationObj.role1);
+            session.pendingConversations.push(conversationObj);
+          }
+        } else if(conversationStatus === true) {
+          if(session.catSent.indexOf(conversationObj.id) === -1) {
+            session.catSent.push(conversationObj.id);
+            session.enqueue(`conversation_cat ${conversationObj.id}`, 0);
+          }
+        } else if(!empty(conversationStatus, 'string')) {
+          // conversationStatus is conversationKeyWrapped
+          // response to conversation_set_key will be conversation_ls
+          session.enqueue(`conversation_set_key ${conversationObj.id} ${conversationStatus}`, 0);
+        }
+      });
+      promises.push(promise);
+    });
+    Promise.all(promises).then(promiseResultArr => {
+      if(keysToRequest.length > 0) {
+        session.enqueue('retrieve_keys_other ' + keysToRequest.join(';'));
+      }
+    });
+  },
+
+  conversation_cat(commandResults, session) {
+    const pastMessages = JSON.parse(commandResults);
+    if(pastMessages.length > 0) {
+      const conversationID = parseInt(pastMessages[0].split(';')[0]);
+      const conversation = session.conversations.find(e => (e.id === conversationID));
+      if(conversation == null) {
+        throw new Error('Unknown conversation');
+      }
+      pastMessages.forEach(pastMsg => {
+        const msgObj = { id: conversationID };
+        const msgFields = pastMsg.split(';', 6);
+        const [id, from, time, contentType, iv, hmac] = msgFields;
+        if(empty(id, 'string') || empty(from, 'string') || empty(time, 'string') ||
+                empty(contentType, 'string') || empty(iv, 'string') || empty(hmac, 'string')) {
+          return;
+        }
+        if(parseInt(id) !== conversationID) {
+          return;
+        }
+        const ciphertext = pastMsg.substring(msgFields.reduce((acc, cur) => (acc + cur.length), 0) + 6);
+        msgObj.from = from;
+        msgObj.time = parseInt(time);
+        msgObj.contentType = contentType;
+        conversation.cipher.decrypt(
+          base64decodebytes(iv), base64decodebytes(hmac), base64decodebytes(ciphertext)
+        ).then(msgData => {
+          msgObj.data = msgData;
+
+          let msgPushed = false;
+          if(!empty(session.externalMessageHandlers, 'object') &&
+                  !empty(session.externalMessageHandlers.conversation_cat, 'function')) {
+            msgPushed = session.externalMessageHandlers.conversation_cat(conversation, msgObj, session.messages);
+          }
+          if(!msgPushed) {
+            session.messages.push(msgObj);
+          }
+        });
+      });
+    }
+  },
+
+  retrieve_keys_self(commandResults, session) {
+    const keyset = JSON.parse(commandResults);
+    session.keysets.push(keyset);
+  },
+
+  retrieve_keys_other(commandResults, session) {
+    const otherKeysets = JSON.parse(commandResults);
+    otherKeysets.forEach(keyset => {
+      session.keysets.push(keyset);
+      const conversationObj = session.pendingConversations.find(e => (keyset.user === e.role1));
+      if(conversationObj != null) {
+        session.addConversationFromLS(conversationObj).then(conversationStatus => {
+          if(conversationStatus === false) {
+            throw new Error('retrieve_keys_self must be called before retrieve_keys_other');
+          } else if(conversationStatus === true) {
+            if(session.catSent.indexOf(conversationObj.id) === -1) {
+              session.catSent.push(conversationObj.id);
+              session.pendingConversations = session.pendingConversations.filter(e => (keyset.user !== e.role1));
+              session.enqueue(`conversation_cat ${conversationObj.id}`, 0);
+            }
+          } else if(!empty(conversationStatus, 'string')) {
+            // conversationStatus is conversationKeyWrapped
+            // response to conversation_set_key will be conversation_ls
+            session.pendingConversations = session.pendingConversations.filter(e => (keyset.user !== e.role1));
+            session.enqueue(`conversation_set_key ${conversationObj.id} ${conversationStatus}`, 0);
+          }
+        });
+      }
+    });
+  },
+
+  set_preferences(commandResults, session) {
+    const preferences = JSON.parse(commandResults);
+    if(!empty(session.externalMessageHandlers, 'object') &&
+            !empty(session.externalMessageHandlers.set_preferences, 'function')) {
+      session.externalMessageHandlers.set_preferences(preferences);
+    }
+  },
+
+  user_message(currentMsg, session) {
+    // user
+    // conversationID;from;time;classes;iv;hmac;messageData
+    const msgObj = {};
+    const msgFields = currentMsg.split(';', 6);
+    const conversationID = parseInt(msgFields[0]);
+    const [from, time, contentType, iv, hmac] = msgFields.slice(1);
+    if(empty(from, 'string') || empty(time, 'string') || empty(contentType, 'string') ||
+            empty(iv, 'string') || empty(hmac, 'string')) {
+      return;
+    }
+    if(conversationID <= 0) {
+      return;
+    }
+    const conversation = session.conversations.find(e => (e.id === conversationID));
+    if(conversation == null) {
+      throw new Error('Unknown conversation');
+    }
+    const ciphertext = currentMsg.substring(msgFields.reduce((acc, cur) => (acc + cur.length), 0) + 6);
+    msgObj.id = conversationID;
+    msgObj.from = from;
+    msgObj.time = parseInt(time);
+    msgObj.contentType = contentType;
+    conversation.cipher.decrypt(
+      base64decodebytes(iv), base64decodebytes(hmac), base64decodebytes(ciphertext)
+    ).then(msgData => {
+      msgObj.data = msgData;
+
+      let msgPushed = false;
+      if(!empty(session.externalMessageHandlers, 'object') &&
+              !empty(session.externalMessageHandlers.user_message, 'function')) {
+        msgPushed = session.externalMessageHandlers.user_message(conversation, msgObj, session.messages);
+      }
+      if(!msgPushed) {
+        session.messages.push(msgObj);
+      }
+    });
+  }
+};
+
 class ChatSession {
-  constructor(conn, messageHandlerCallback) {
+  constructor(conn, externalMessageHandlers) {
     if(!isValidUUID(conn.data)) {
       throw new Error('Not connected');
     }
     this.uuid = conn.data;
+
     this.conversations = [];
     this.keysets = [];
     this.messages = [];
@@ -12,9 +172,11 @@ class ChatSession {
     this.catSent = [];
     this.keyWrapper = null;
 
+    this.externalMessageHandlers = externalMessageHandlers;
+
     this.websocket = new WebSocket(`wss://${window.location.hostname}:8082/${this.uuid}`);
     this.websocket.onmessage = (m) => {
-      messageHandlerCallback(m.data, this);
+      this.internalMessageHandler(m.data);
     };
   }
 
@@ -153,16 +315,40 @@ class ChatSession {
       return conversationKeyWrapped;  // Message handler should upload wrapped key to the server
     }
   }
+
+  internalMessageHandler(message) {
+    console.log(message);
+    let [conversationID, messageType] = message.split(';', 2);
+    conversationID = parseInt(conversationID);
+    if(conversationID === 0) {
+      // server
+      // conversationID;messageType;messageData
+      if(messageType === 'command') {
+        const messageData = message.substring(conversationID.toString().length + messageType.length + 2);
+        const commandIdx = messageData.indexOf(';');
+        const command = messageData.substring(0, commandIdx);
+        const commandResults = messageData.substring(commandIdx + 1);
+        const commandHandler = commandHandlers[command];
+        if(!empty(commandHandler, 'function')) {
+          commandHandler(commandResults, this);
+        }
+      }
+    } else {
+      // user
+      // conversationID;from;time;classes;iv;hmac;messageData
+      commandHandlers.user_message(message, this);
+    }
+  }
 }
 
-function chatClientBegin(messageHandler) {
+function chatClientBegin(externalMessageHandlers) {
   return new Promise((resolve2, reject2) => {
     new Promise((resolve, reject) => {
       axios.post('/backend-chat.php', { command: 'join' })
         .then((response) => { resolve(response.data); });
     }).then(uuid => {
-      const ref = new ChatSession(uuid, messageHandler);
-      ref.keyWrapper = new CipherStore(base64decodebytes(sessionStorage.getItem('keyWrapper')), false);
+      const ref = new ChatSession(uuid, externalMessageHandlers);
+      ref.keyWrapper = new CipherStore(base64decodebytes(localStorage.getItem('keyWrapper')), false);
       ref.keyWrapper.readyPromise.then(() => {
         ref.enqueue('retrieve_keys_self', 0)
           .then(() => ref.enqueue('conversation_ls', 0))
--- ChatClient/src/example.js
+++ ChatClient/src/example.js
@@ -3,179 +3,39 @@
 //
 //
 
-const commandHandlers = {
-  conversation_request(commandResults, session) {
-    const conversationRequest = JSON.parse(commandResults);
-    session.addConversationFromRequest(conversationRequest).then(toAdd => {
-      session.enqueue('conversation_add ' + JSON.stringify(toAdd), 0);
-    });
+const handlers = {
+  conversation_cat(conversation, pastMsgObj, processedArr) {
+    // Code to display the message to the screen goes here
+    // pastMsgObj.id gives the conversation ID of the message
+    //           .from gives the username of the sender
+    //           .time gives the timestamp (result of Date.now() when message was sent)
+    //           .data gives the typed message
+    //           .contentType gives space-separated classes that describe how to render the message
+    //               ('markdown' or 'plaintext')
+
+    // must return true if processedArr has been updated, and false otherwise
+    return false;
   },
 
-  conversation_ls(commandResults, session) {
-    const conversationLS = JSON.parse(commandResults);
-    const keysToRequest = [];
-    const promises = [];
-    conversationLS.forEach(conversationObj => {
-      const promise = session.addConversationFromLS(conversationObj).then(conversationStatus => {
-        if(conversationStatus === false) {
-          if(keysToRequest.indexOf(conversationObj.role1) === -1) {
-            keysToRequest.push(conversationObj.role1);
-            session.pendingConversations.push(conversationObj);
-          }
-        } else if(conversationStatus === true) {
-          if(session.catSent.indexOf(conversationObj.id) === -1) {
-            session.catSent.push(conversationObj.id);
-            session.enqueue(`conversation_cat ${conversationObj.id}`, 0);
-          }
-        } else if(!empty(conversationStatus, 'string')) {
-          // conversationStatus is conversationKeyWrapped
-          // response to conversation_set_key will be conversation_ls
-          session.enqueue(`conversation_set_key ${conversationObj.id} ${conversationStatus}`, 0);
-        }
-      });
-      promises.push(promise);
-    });
-    Promise.all(promises).then(promiseResultArr => {
-      if(keysToRequest.length > 0) {
-        session.enqueue('retrieve_keys_other ' + keysToRequest.join(';'));
-      }
-    });
-  },
-
-  conversation_cat(commandResults, session) {
-    const pastMessages = JSON.parse(commandResults);
-    if(pastMessages.length > 0) {
-      const conversationID = parseInt(pastMessages[0].split(';')[0]);
-      const conversation = session.conversations.find(e => (e.id === conversationID));
-      if(conversation == null) {
-        throw new Error('Unknown conversation');
-      }
-      pastMessages.forEach(pastMsg => {
-        const msgObj = { id: conversationID };
-        const msgFields = pastMsg.split(';', 6);
-        const [id, from, time, contentType, iv, hmac] = msgFields;
-        if(empty(id, 'string') || empty(from, 'string') || empty(time, 'string') ||
-                empty(contentType, 'string') || empty(iv, 'string') || empty(hmac, 'string')) {
-          return;
-        }
-        if(parseInt(id) !== conversationID) {
-          return;
-        }
-        const ciphertext = pastMsg.substring(msgFields.reduce((acc, cur) => (acc + cur.length), 0) + 6);
-        msgObj.from = from;
-        msgObj.time = parseInt(time);
-        conversation.cipher.decrypt(
-          base64decodebytes(iv), base64decodebytes(hmac), base64decodebytes(ciphertext)
-        ).then(msgData => {
-          msgObj.data = msgData;
-          session.messages.push(msgObj);
-
-          // Code to display the message to the screen goes here
-          // Should depend on the message classes `contentType`, which is a
-          // space separated string specifying the type of message (always 'user')
-          // and the sender's preferred format ('markdown' or 'plaintext')
-        });
-      });
-    }
-  },
-
-  retrieve_keys_self(commandResults, session) {
-    const keyset = JSON.parse(commandResults);
-    session.keysets.push(keyset);
-  },
-
-  retrieve_keys_other(commandResults, session) {
-    const otherKeysets = JSON.parse(commandResults);
-    otherKeysets.forEach(keyset => {
-      session.keysets.push(keyset);
-      const conversationObj = session.pendingConversations.find(e => (keyset.user === e.role1));
-      if(conversationObj != null) {
-        session.addConversationFromLS(conversationObj).then(conversationStatus => {
-          if(conversationStatus === false) {
-            throw new Error('retrieve_keys_self must be called before retrieve_keys_other');
-          } else if(conversationStatus === true) {
-            if(session.catSent.indexOf(conversationObj.id) === -1) {
-              session.catSent.push(conversationObj.id);
-              session.pendingConversations = session.pendingConversations.filter(e => (keyset.user !== e.role1));
-              session.enqueue(`conversation_cat ${conversationObj.id}`, 0);
-            }
-          } else if(!empty(conversationStatus, 'string')) {
-            // conversationStatus is conversationKeyWrapped
-            // response to conversation_set_key will be conversation_ls
-            session.pendingConversations = session.pendingConversations.filter(e => (keyset.user !== e.role1));
-            session.enqueue(`conversation_set_key ${conversationObj.id} ${conversationStatus}`, 0);
-          }
-        });
-      }
-    });
-  },
-
-  set_preferences(commandResults, session) {
-    const preferences = JSON.parse(commandResults);
+  set_preferences(prefObj) {
     // Code to change the user's preferences goes here
     // Currently the only preference is `markdown: Boolean`,
     // which changes the default formatting of messages *sent* by the user
   },
 
-  user_message(currentMsg, session) {
-    // user
-    // conversationID;from;time;classes;iv;hmac;messageData
-    const msgObj = {};
-    const msgFields = currentMsg.split(';', 6);
-    const conversationID = parseInt(msgFields[0]);
-    const [from, time, contentType, iv, hmac] = msgFields.slice(1);
-    if(empty(from, 'string') || empty(time, 'string') || empty(contentType, 'string') ||
-            empty(iv, 'string') || empty(hmac, 'string')) {
-      return;
-    }
-    if(conversationID <= 0) {
-      return;
-    }
-    const conversation = session.conversations.find(e => (e.id === conversationID));
-    if(conversation == null) {
-      throw new Error('Unknown conversation');
-    }
-    const ciphertext = currentMsg.substring(msgFields.reduce((acc, cur) => (acc + cur.length), 0) + 6);
-    msgObj.id = conversationID;
-    msgObj.from = from;
-    msgObj.time = parseInt(time);
-    conversation.cipher.decrypt(
-      base64decodebytes(iv), base64decodebytes(hmac), base64decodebytes(ciphertext)
-    ).then(msgData => {
-      msgObj.data = msgData;
-      session.messages.push(msgObj);
+  user_message(conversation, msgObj, processedArr) {
+    // Code to display the message to the screen goes here
+    // msgObj.id gives the conversation ID of the message
+    //       .from gives the username of the sender
+    //       .time gives the timestamp (result of Date.now() when message was sent)
+    //       .data gives the typed message
+    //       .contentType gives space-separated classes that describe how to render the message
+    //           ('markdown' or 'plaintext')
 
-      // Code to display the message to the screen goes here
-      // Should depend on the message classes `contentType`, which is a
-      // space separated string specifying the type of message (always 'user')
-      // and the sender's preferred format ('markdown' or 'plaintext')
-    });
+    // must return true if processedArr has been updated, and false otherwise
+    return false;
   }
 };
 
-function messageHandler(message, session) {
-  console.log(message);
-  let [conversationID, messageType] = message.split(';', 2);
-  conversationID = parseInt(conversationID);
-  if(conversationID === 0) {
-    // server
-    // conversationID;messageType;messageData
-    if(messageType === 'command') {
-      const messageData = message.substring(conversationID.toString().length + messageType.length + 2);
-      const commandIdx = messageData.indexOf(';');
-      const command = messageData.substring(0, commandIdx);
-      const commandResults = messageData.substring(commandIdx + 1);
-      const commandHandler = commandHandlers[command];
-      if(!empty(commandHandler, 'function')) {
-        commandHandler(commandResults, session);
-      }
-    }
-  } else {
-    // user
-    // conversationID;from;time;classes;iv;hmac;messageData
-    commandHandlers.user_message(message, session);
-  }
-}
-
 let session;
-chatClientBegin(messageHandler).then(s => { session = s; });
+chatClientBegin(handlers).then(s => { session = s; });
