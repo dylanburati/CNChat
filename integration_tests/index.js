b45b6ff42e8d1375974ad67131920ece5ceb544a
Dylan Burati <dylanburati@protonmail.com>
2019-03-12 21:18:13 -0400

    CircleCI test 2

--- integration_tests/index.js
+++ integration_tests/index.js
@@ -1,12 +1,33 @@
 const stdOut = document.getElementById('stdout');
 
+async function generateKeyWrapper(pass) {
+  const passUTF8 = toUTF8Bytes(pass);
+  const securePad = [];
+
+  const inHashBuf = new ArrayBuffer(passUTF8.length);
+  const inHashBufV = new Uint8Array(inHashBuf);
+  for(let i = 0; i < passUTF8.length; i++) {
+    inHashBufV[i] = passUTF8[i];
+  }
+  const hash = await window.crypto.subtle.digest('sha-256', inHashBuf);
+  const hashV = new Uint8Array(hash);
+
+  const keyWrapper = new CipherStore(hashV, true);
+  await keyWrapper.readyPromise;
+  return {
+    storage: base64encodebytes(hashV),
+  };
+}
+
 function println(line) {
   stdOut.textContent += line + '\n';
 }
 
-chatClientBegin({}, 'http://localhost:8081', 'join a')
-  .then(session => {
-    println(session.uuid);  
-  });
-
-
+let credentials = {};
+let session;
+async function test() {
+  const keyWrapper = await generateKeyWrapper(credentials.pass);
+  localStorage.setItem('keyWrapper', keyWrapper.storage);
+  session = await chatClientBegin({}, 'https://localhost:8081', `join ${credentials.name}`);
+  println(session.uuid);
+}
--- integration_tests/setup.sh
+++ integration_tests/setup.sh
@@ -1,4 +1,14 @@
 DB_USER=$(whoami)
+WORKING_DIR=$(pwd)
+cd $HOME
+
+[ -r ssl.conf ] || sed '/^\[ v3_ca \]/ a\subjectAltName=DNS:localhost' /etc/ssl/openssl.cnf > ssl.conf
+[ -r cert.jks ] ||
+    openssl req -newkey rsa:4096 -nodes -batch -keyout cert.key -x509 -days 90 -config ssl.conf -out cert.crt &&
+    openssl pkcs12 -export -in cert.crt -inkey cert.key -out tmp.p12 -password "pass:1nopassword" -name localhost &&
+    keytool -importkeystore -deststorepass "nopassword" -destkeypass "nopassword" -destkeystore cert.jks -srckeystore tmp.p12 -srcstoretype PKCS12 -srcstorepass "1nopassword" -alias "localhost"
+
+cd $WORKING_DIR
 echo '{
     "keyStoreLocation": "/'$HOME'/cert.jks",
   
@@ -6,6 +16,7 @@ echo '{
     "mariaDBPassword": "",
     "database": "testing"
 }' > bin/config.json
+
 echo "CREATE DATABASE testing;
 USE testing;
 CREATE TABLE \$keystore (
@@ -18,5 +29,8 @@ CREATE TABLE \$keystore (
 );
 CREATE USER '$DB_USER'@'127.0.0.1' IDENTIFIED BY '';
 GRANT ALL ON testing.* TO '$DB_USER'@'127.0.0.1';" | sudo mysql -u root --protocol=tcp
-mysql -u $DB_USER -e "SELECT * FROM \$keystore" testing || true
-java -jar bin/ChatServer.jar
+
+echo "function credentials() {
+  return $CREDENTIAL_JSON;
+}" >> integration_tests/chat.spec.js
+
